e4c8667a5ff9de5fc7aa0c125464987268e87a67
subhojit <goswamisubhojit1947@gmail.com>
2019-01-13 18:38:08 +0530

    class inheritance

--- object.js
+++ object.js
@@ -18,8 +18,17 @@ class User {
         return this;//need to return the scope of this in order to do method chaining
     }
 }
+
+class Admin extends User {
+    deleteUser(user) {
+        users = users.filter(u => {
+            return u.email !== user.email;
+        })
+    }
+}
 var userOne = new User('goswamisubhojit1947@gmail.com', 'subho', 0);
 var userTwo = new User('substallin@gmail.com', 'stallin', 0);
+var admin = new Admin('goswamibiswajit891@yahoo.in', 'biswajit', 0);
 console.log(this.userOne);
 console.log(this.userTwo);
 this.userOne.logIn();
@@ -29,3 +38,9 @@ this.userTwo.logOut();
 this.userOne.logIn().updateScore().logOut();//this is method chaining
 this.userOne.logIn().updateScore().logOut();
 this.userOne.logIn().updateScore().logOut();
+
+var users = [userOne, userTwo, admin];
+
+admin.deleteUser(userTwo);
+console.log(users);
+console.log(users);
