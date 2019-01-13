237dc66225a9588904365ddbfeececb52874c48c
subhojit <goswamisubhojit1947@gmail.com>
2019-01-13 16:42:13 +0530

    class-methods

--- object.js
+++ object.js
@@ -3,8 +3,18 @@ class User {
         this.email = email;
         this.name = name;
     }
+    logOut() {
+        console.log(this.name + " has logged out");
+    }
+    logIn() {
+        console.log(this.name + " has logged in ");
+    }
 }
 var userOne = new User('goswamisubhojit1947@gmail.com', 'subho');
 var userTwo = new User('substallin@gmail.com', 'stallin');
 console.log(this.userOne);
 console.log(this.userTwo);
+this.userOne.logIn();
+this.userOne.logOut();
+this.userTwo.logIn();
+this.userTwo.logOut();
