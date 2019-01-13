212c7eb5efaa371a4da1dd9f2827cdab3c82d240
subhojit <goswamisubhojit1947@gmail.com>
2019-01-13 19:55:37 +0530

    prototype

--- object.js
+++ object.js
@@ -2,9 +2,16 @@ function User(email, name) { //this is how basically the constructor that we spe
     this.email = email;
     this.name = name;
     this.online = false;
-    this.login = function() {
-        console.log(this.email + " has logged in ");
-    }
+}
+
+User.prototype.login = function() {
+    this.online = true;
+    console.log(this.email + " has logged in ");
+}
+
+User.prototype.logout = function() {
+    this.online = false;
+    console.log(this.email + " has logged out ");
 }
 
 var userOne = new User('goswamisubhojit1947@gmail.com', 'subho');
