6ae25457018e81061ccd17b2db11a820a7db58b8
subhojit <goswamisubhojit1947@gmail.com>
2019-01-13 17:01:03 +0530

    method-chaining

--- object.js
+++ object.js
@@ -1,20 +1,31 @@
 class User {
-    constructor (email, name) {
+    constructor (email, name, score) {
         this.email = email;
         this.name = name;
+        this.score = score;
     }
     logOut() {
         console.log(this.name + " has logged out");
+        return this;
     }
     logIn() {
         console.log(this.name + " has logged in ");
+        return this;
+    }
+    updateScore() {
+        console.log(this.name + " has " + this.score + " score");
+        this.score++;
+        return this;//need to return the scope of this in order to do method chaining
     }
 }
-var userOne = new User('goswamisubhojit1947@gmail.com', 'subho');
-var userTwo = new User('substallin@gmail.com', 'stallin');
+var userOne = new User('goswamisubhojit1947@gmail.com', 'subho', 0);
+var userTwo = new User('substallin@gmail.com', 'stallin', 0);
 console.log(this.userOne);
 console.log(this.userTwo);
 this.userOne.logIn();
 this.userOne.logOut();
 this.userTwo.logIn();
 this.userTwo.logOut();
+this.userOne.logIn().updateScore().logOut();//this is method chaining
+this.userOne.logIn().updateScore().logOut();
+this.userOne.logIn().updateScore().logOut();
